4e0ef6b7c13ce102a9e96694cb6d1f8901a6cbdf
neokarn <34811101+neokarn@users.noreply.github.com>
2020-12-13 08:32:01 +0700

    Create lab5_2.cpp

---
+++ lab5_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+    cout << "Enter length of the first vector: ";
+    cin >> l1;
+    cout << "Enter direction of the first vector (deg): ";
+    cin >> a1;
+    cout << "Enter length of the second vector: ";
+    cin >> l2;
+    cout << "Enter direction of the second vector (deg): ";
+    cin >> a2;
+    
+    a1 = deg2rad(a1);
+    a2 = deg2rad(a2);
+    xcomp = findXComponent(l1,l2,a1,a2);
+    ycomp = findYComponent(l1,l2,a1,a2);
+    result_vec_length = pythagoras(xcomp,ycomp);
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
+
+    showResult(result_vec_length,result_vec_direction);
+}
