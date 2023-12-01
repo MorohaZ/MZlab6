06413f14669737669a5087fdde64b99e51fe69d8
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2023-12-01 15:12:18 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include <bits\stdc++.h>
+#include<cmath>
 
 using namespace std;
+
+double deg2rad(double deg){
+return deg*3.14/180;
+}
+
+double rad2deg(double rad){
+return rad*180/3.14;
+}
+
+double findXComponent(double L1,double L2, double A1,double A2){
+return (L1*cos(A1))+(L2*cos(A2));
+}
+
+double findYComponent(double L1,double L2, double A1,double A2){
+    return (L1*sin(A1))+(L2*sin(A2));
+}
+
+double  pythagoras(double xcop,double ycop){
+double length_tan = sqrt(pow(xcop,2)+pow(ycop,2));
+return length_tan;
+}
+
+double showResult(double length_vec,double direction_vec){
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+cout << "Length of the resultant vector = "<<length_vec<<"\n";
+cout<<"Direction of the resultant vector (deg) = "<<direction_vec<<"\n"; 
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
